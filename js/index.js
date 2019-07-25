a716ef07784246e520b62b8ec11fff67b6f2bff8
hdd <1263823818@qq.com>
2019-07-25 17:11:23 +0800

    使用jQuery入口函数

--- js/index.js
+++ js/index.js
@@ -18,8 +18,7 @@ function daochu(){
 
 
 //主入口函数 调用事件处理模块的函数
-window.onload = function () {
-
+$(function(){
     m_event.onloadevent();  //注册onload事件
 
     m_event.moveinout(); //响应主体内容框中所有图标的鼠标移入移出事件
@@ -31,4 +30,19 @@ window.onload = function () {
     m_event.youjian(); //处理所有鼠标右键事件
 
     m_event.scroll();  //鼠标滚动事件响应
-}
+})
+
+// window.onload = function () {
+
+//     m_event.onloadevent();  //注册onload事件
+
+//     m_event.moveinout(); //响应主体内容框中所有图标的鼠标移入移出事件
+
+//     m_event.onclickevent(); //处理所有的点击事件
+
+//     m_event.keydown(); //处理所有回车事件
+
+//     m_event.youjian(); //处理所有鼠标右键事件
+
+//     m_event.scroll();  //鼠标滚动事件响应
+// }
